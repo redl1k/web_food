92008ff4a26e9b6b895009ef14b2d130de7a4d52
redl1k <andrian14756@gmail.com>
2022-11-10 20:22:30 +0300

    upd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-
+import { HideHeaderDirective } from './../directives/hide-header.directive';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  categories = [];
+  highlights = [];
+  featured = [];
 
-  constructor() {}
+  catSlideOpts = {
+    freeMode: true,
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10
+  };
+  highlightSlideOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true
+  }
 
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json').subscribe((res: any) => {
+        console.log((res));  
+      this.categories = res.categories;
+        this.highlights = res.highlights;
+        this.featured = res.featured;
+    });
+  }
 }
+
