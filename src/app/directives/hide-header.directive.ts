786d0100689795ef8fd007f347b6fef983dc69d9
redl1k <andrian14756@gmail.com>
2022-12-03 20:25:29 +0300

    upd

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
-
+import { Directive, AfterViewInit, HostListener, Input, Renderer2 } from '@angular/core';
+import { isPlatform, DomController } from '@ionic/angular';
 @Directive({
   selector: '[appHideHeader]'
 })
-export class HideHeaderDirective {
+export class HideHeaderDirective implements AfterViewInit{
+  @Input('appHideHeader') header: any;
+  private headerHeight = isPlatform('ios') ? 44:56;
+  private children: any;
 
-  constructor() { }
+
+  constructor(
+    private renderer: Renderer2,
+    private domCtrl: DomController,
+  ){}
+  ngAfterViewInit(): void {
+    this.header = this.header.el;
+    this.children = this.header.children;
+  }
+
+  @HostListener('ionScroll', ['$event']) onContentScroll($event){
+
+  }
 
 }
