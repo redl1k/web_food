881c7e61a433a3eb9016ccd740c809a0c0e7c61e
redl1k <andrian14756@gmail.com>
2022-12-04 01:06:11 +0300

    making moving image

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, QueryList, ViewChild, } from '@angular/core';
 import { IonContent, IonList, IonSlides, isPlatform } from '@ionic/angular';
-
+import { ParallaxDirective } from '../directives/parallax.directive';
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
