0ac9f34ca342a3f7b98b3fa3aca688a3d59c9d07
redl1k <andrian14756@gmail.com>
2022-12-04 01:57:55 +0300

    finnal

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, QueryList, ViewChild, } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { IonContent, IonList, IonSlides, isPlatform } from '@ionic/angular';
 import { ParallaxDirective } from '../directives/parallax.directive';
 @Component({
@@ -10,14 +10,46 @@ import { ParallaxDirective } from '../directives/parallax.directive';
 })
 export class DetailsPage implements OnInit, AfterViewInit {
   data = null;
-  constructor(private http: HttpClient) { }
+
+  opts = {
+    freeMode: true,
+    slidesPerView: 2.6,
+    slidesOffsetBefore: 30,
+    slidesOffsetAfter: 100
+  }
+
+  categorySlidesVisible = false;
+  activeCategory = 0;
+  @ViewChildren(IonList, { read: ElementRef}) lists: QueryList<ElementRef>;
+  listElements: [];
+  @ViewChild(IonContent) content: IonContent;
+
+
+  constructor(private http: HttpClient,
+    @Inject(DOCUMENT) private document: Document) { }
 
 	ngOnInit() {
 			this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/1.json').subscribe((res: any) => {
 				this.data = res;
         console.log(res);
 			});
-	}
+      const headerHeight = isPlatform('ios') ? 44:56;
+      this.document.documentElement.style.setProperty('--header-position', `calc(env(safe-area-inset-top) + ${headerHeight}px)`)
+    }
 
-	ngAfterViewInit() {}
+	ngAfterViewInit() {
+    // this.lists.changes.subscribe( => {
+    //   this.listElements = this.lists.toArray();
+    // });
+  }
+
+  selectCategory(index){
+    // const child = this.listElements[index].nativeElement;
+    // this.content.scrollToPoint(0, child.offsetTop -120, 1000);
+  }
+
+  onScroll(ev){
+    const offset = ev.detail.scrollTop;
+    this.categorySlidesVisible = offset > 500;
+  }
 }
